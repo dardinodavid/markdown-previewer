7cee42a1b47275ba8b163eacd9ff9d2b6fce0460
David Andrews <contactdardino@gmail.com>
2019-02-06 12:49:16 +0000

    Updates

--- precache-manifest.afc6c883df707cefa61625dbdd6936a3.js
+++ precache-manifest.afc6c883df707cefa61625dbdd6936a3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
   },
   {
-    "revision": "f1048360a23d242af58f",
-    "url": "/markdown-previewer/static/js/main.f1048360.chunk.js"
+    "revision": "b937e0d27c9bdf30025d",
+    "url": "/markdown-previewer/static/js/main.b937e0d2.chunk.js"
   },
   {
     "revision": "6e85f47a5daa64f7d76b",
     "url": "/markdown-previewer/static/js/1.6e85f47a.chunk.js"
   },
   {
-    "revision": "f1048360a23d242af58f",
-    "url": "/markdown-previewer/static/css/main.8f77a61d.chunk.css"
+    "revision": "b937e0d27c9bdf30025d",
+    "url": "/markdown-previewer/static/css/main.006b2d13.chunk.css"
   },
   {
-    "revision": "ede7a264ad29794e3c6d52b6f042fdec",
+    "revision": "4dcb9f5ad2386cc6e42ed5b036bded6c",
     "url": "/markdown-previewer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/markdown-previewer/precache-manifest.f45964c02a305349515ad50bd37f8761.js"
+  "/markdown-previewer/precache-manifest.afc6c883df707cefa61625dbdd6936a3.js"
 );
 
 workbox.clientsClaim();
